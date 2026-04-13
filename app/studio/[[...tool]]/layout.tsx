3da3a2dd8e701ce6719457113710c8356c4669b0
Ellie Holt <eleanorholt97@gmail.com>
2026-04-13 16:04:19 +0100

    Connect studio to sanity dashboard

--- app/studio/[[...tool]]/layout.tsx
+++ app/studio/[[...tool]]/layout.tsx
@@ -1,7 +1,17 @@
+import { preloadModule } from "react-dom";
+
+const bridgeScript = "https://cdn.sanity-cdn.com/bridge.js";
+
 export default function StudioLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+  preloadModule(bridgeScript, { as: "script" });
+  return (
+    <>
+      <script src={bridgeScript} async type="module" />
+      {children}
+    </>
+  );
 }
---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: "jtd931ew",
+    dataset: "production",
+  },
+});
